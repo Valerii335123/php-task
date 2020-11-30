bd30a231212dee0aedb7e14a354845b43d3fd731
Valerii <valerasobco@gmail.com>
2020-11-30 22:13:22 +0200

    Add last change

--- r.php
+++ r.php
@@ -8,7 +8,42 @@ $rez=mysqli_fetch_row($r);
 
 if($rez[3]) {
 	
-	print_r($rez);
+	//print_r($rez);
 	}
 	else header('Location: home.php');
   ?>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">	
+<table class="table">
+	 <thead class="thead-dark">
+	    <tr>
+	    
+	      <th scope="col" width="25%">title</th>
+	      <th scope="col">text</th>
+	      <th scope="col" width="10px">Active</th>
+	      
+	    </tr>
+	  </thead>	
+
+
+
+
+<?php
+echo "<tr>";
+
+	 	
+	 	echo "<td>$rez[1]</td>";
+	 	echo "<td>$rez[2]</td>";
+	 	echo ($rez[3])?'<td>
+	 		<p class="btn btn-success" >Active</p>
+	 	</td>':'<td>
+	 		<p class="btn btn-danger">Not Active</p>
+	 	</td>';
+	 	
+
+	 	
+?>
+
+
+
+	</table>>
